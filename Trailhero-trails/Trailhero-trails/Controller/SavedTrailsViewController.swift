17f3aa813efdb2d436d30fcea4fb2d8831147015
Toni Itkonen <toni.itkonen@icloud.com>
2021-06-24 12:13:17 +0300

    save-text to CoreData added

--- Trailhero-trails/Trailhero-trails/Controller/SavedTrailsViewController.swift
+++ Trailhero-trails/Trailhero-trails/Controller/SavedTrailsViewController.swift
@@ -1,43 +1,92 @@
 import UIKit
+import CoreData
 
 class SavedTrailsViewController: UITableViewController {
 
+    var trails = [Trails]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // Context Layer
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadTrails()
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-        // return categories.count
+        // return the number of rows
+        return trails.count
     }
+    
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SavedTrailsCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = trails[indexPath.row].title
 
         return cell
     }
-    */
+ 
 
+    // MARK: - Add New text
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New text", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newTrails = Trails(context: self.context)
+            newTrails.title = textField.text!
 
+            self.trails.append(newTrails)
+            
+            self.saveTrails()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new text"
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
     
+    // MARK: - CRUD Functions
+    
+    func saveTrails() {
+         
+         do {
+            try context.save()
+         } catch {
+            print("Error saving context \(error)")
+         }
+         tableView.reloadData()
+    }
+    
+    func loadTrails() {
+        
+        let request : NSFetchRequest<Trails> = Trails.fetchRequest()
+        
+        do {
+            trails = try context.fetch(request)
+        } catch {
+            print("Error Fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
 
     
 
